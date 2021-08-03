d28de9645fa6ebc10d09c47f45157b85dc72db50
AnthonyCavalcante <88403141+AnthonyCavalcante@users.noreply.github.com>
2021-08-03 17:07:45 -0300

    Projeto 3 do SoloLearn

---
+++ sololearn-projects.py
@@ -0,0 +1,14 @@
+n=input("insira um valor:")
+lista=list(range(1,int(n)))
+print("os valores são "+str(lista))
+for valor in lista:
+    if valor%2==0:
+        continue
+    elif valor%3==0 and valor%5==0:
+        print("sololearn")
+    elif valor%5==0:
+        print("learn")
+    elif valor%3==0:
+        print("solo")
+    else:
+        print(valor)
